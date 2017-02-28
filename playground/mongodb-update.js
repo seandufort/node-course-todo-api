b550a3494fdb616a1303397fa73fa45ad9ee1a61
Sean Dufort <Sean.Dufort@ge.com>
2017-02-28 13:24:07 -0500

    Add update script'

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID("58b5a26ad02f694b02c47bc3")
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID("58b57f757b946b1b30596b63")
+  }, {
+    $set: {
+      name: 'Sean Dufort'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  //db.close();
+});
