cd8c6d0f8cf5b210e5ed04a91a0c96da2d597029
Sean Dufort <Sean.Dufort@ge.com>
2017-03-10 08:26:56 -0500

    Commenting of Delete and Patch to attempt to locate problem

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const {ObjectID} = require('mongodb');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const _ = require('lodash');
+//const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -50,48 +50,48 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.delete('/todos/:id', (req, res) => {
-  var id = req.params.id;
-
-  if(!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if(!todo) {
-      return res.status(404).send();
-    }
-    res.send({todo});
-  }).catch((e) => {
-    res.status(400).send();
-  });
-});
-
-app.patch('/todos/:id', (req, res) => {
-  var id = req.params.id;
-  var body = _.pick(req.body, ['text', 'completed']);
-
-  if(!ObjectID.isValid(id)) {
-    return res.staus(404).send();
-  }
-
-  if(_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
-  } else {
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-    if(!todo) {
-      return res.status(400).send();
-    }
-
-    res.send({todo});
-  }).catch((e) => {
-    res.status(400).send();
-  });
-});
+// app.delete('/todos/:id', (req, res) => {
+//   var id = req.params.id;
+//
+//   if(!ObjectID.isValid(id)) {
+//     return res.status(404).send();
+//   }
+//
+//   Todo.findByIdAndRemove(id).then((todo) => {
+//     if(!todo) {
+//       return res.status(404).send();
+//     }
+//     res.send({todo});
+//   }).catch((e) => {
+//     res.status(400).send();
+//   });
+// });
+//
+// app.patch('/todos/:id', (req, res) => {
+//   var id = req.params.id;
+//   var body = _.pick(req.body, ['text', 'completed']);
+//
+//   if(!ObjectID.isValid(id)) {
+//     return res.staus(404).send();
+//   }
+//
+//   if(_.isBoolean(body.completed) && body.completed) {
+//     body.completedAt = new Date().getTime();
+//   } else {
+//     body.completed = false;
+//     body.completedAt = null;
+//   }
+//
+//   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+//     if(!todo) {
+//       return res.status(400).send();
+//     }
+//
+//     res.send({todo});
+//   }).catch((e) => {
+//     res.status(400).send();
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
